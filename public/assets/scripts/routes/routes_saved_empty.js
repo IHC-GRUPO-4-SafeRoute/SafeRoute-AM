2e0c6511ecf64d6e87cab24b10029eae9f4eea66
LoGi2 <aldoforest123@gmail.com>
2026-07-09 13:44:50 -0500

    feat: implement safe route nodes

--- public/assets/scripts/routes/routes_saved_empty.js
+++ public/assets/scripts/routes/routes_saved_empty.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const backButton = document.querySelector(".back-button");
+  const favoriteTabButton = document.querySelector(".favorite-tab-button");
   const saveRouteButton = document.querySelector(".save-route-button");
-  const favoritesTab = document.querySelector(".tab-button:first-child");
 
   if (backButton) {
     backButton.addEventListener("click", () => {
@@ -9,15 +9,15 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  if (saveRouteButton) {
-    saveRouteButton.addEventListener("click", () => {
-      window.location.href = "./routes_new_route.html";
+  if (favoriteTabButton) {
+    favoriteTabButton.addEventListener("click", () => {
+      window.location.href = "./routes_frequent_routes.html";
     });
   }
 
-  if (favoritesTab) {
-    favoritesTab.addEventListener("click", () => {
-      window.location.href = "./routes_frequent_routes.html";
+  if (saveRouteButton) {
+    saveRouteButton.addEventListener("click", () => {
+      window.location.href = "./routes_new_route.html";
     });
   }
 });
